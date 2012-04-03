7de7f99723270d66a4390a97e3e956c7f06c96b3
kazim akgul <kazimakgul@gmail.com>
2012-04-04 02:35:15 +0300

    DB eklendi

--- app/Config/database.php
+++ app/Config/database.php
@@ -65,7 +65,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'benimdb',
+		'database' => 'NewDB',
 		'prefix' => '',
 		//'encoding' => 'utf8',
 	);
