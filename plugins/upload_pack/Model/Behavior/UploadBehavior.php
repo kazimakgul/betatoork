e50ca5b566aa5a216e0511e098f365443af9976b
kazim akgul <kazimakgul@gmail.com>
2012-06-02 22:56:26 +0300

    amazon working

--- plugins/upload_pack/Model/Behavior/UploadBehavior.php
+++ plugins/upload_pack/Model/Behavior/UploadBehavior.php
@@ -21,10 +21,10 @@ class UploadBehavior extends ModelBehavior {
     private $toDelete = array();
 
     private $maxWidthSize = false;
-
+    public $webroot = "s3.amazonaws.com/betatoorkpics/";
     public function setup(&$model, $settings = array()) {
         $defaults = array(
-            'path' => ':s3.amazonaws.com/betatoorkpics/upload/:model/:id/:basename_:style.:extension',
+            'path' => ':$webroot/upload/:model/:id/:basename_:style.:extension',
             'styles' => array('showcase' => '640x350','toorksize' => '200x110','thumb' => '100x55'),
             'resizeToMaxWidth' => false,
             'quality' => 75
