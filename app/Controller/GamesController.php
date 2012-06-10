6b4f97de1f6af4673f664a74b41df4fb340e3b17
oguzhan altan <hoaltan@hotmail.com>
2012-06-10 04:46:51 +0300

    Userinfo

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -256,6 +256,30 @@ class GamesController extends AppController {
 	    $this->set('playcount', $playcount);
 
 	}
+	
+	
+	public function usergame_user_panel2($userid=NULL) {
+		$this->loadModel('User');
+		$this->loadModel('Subscription');
+		$this->loadModel('Playcount');
+		$this->layout='base';
+		
+	    $gamenumber = $this->Game->find('count', array('conditions' => array('Game.User_id' => $userid)));
+	    $favoritenumber = $this->Game->Favorite->find('count', array('conditions' => array('Favorite.User_id' => $userid)));
+	    $subscribe = $this->Subscription->find('count', array('conditions' => array('Subscription.subscriber_id' => $userid)));
+	    $subscribeto = $this->Subscription->find('count', array('conditions' => array('Subscription.subscriber_to_id' => $userid)));
+		$playcount = $this->Playcount->find('count', array('conditions' => array('Playcount.user_id' => $userid,'Game.active'=>1)));
+		$user = $this->User->find('first', array('conditions'=> array('User.id'=>$userid)));
+	    $this->set('user',$user);
+
+	    $this->set('userid', $userid);
+	    $this->set('gamenumber', $gamenumber);
+	    $this->set('favoritenumber', $favoritenumber);
+	    $this->set('subscribe', $subscribe);
+	    $this->set('subscribeto', $subscribeto);
+	    $this->set('playcount', $playcount);
+
+	}
 
 	public function play2_user_panel() {
 		$this->loadModel('Subscription');
@@ -337,14 +361,15 @@ public function channelgames() {
 	$this->layout='base';
 	$this->loadModel('User');
 	$this->leftpanel();
-    $this->usergame_user_panel();
+    
     $seo_username = $this->request->params['pass'][0];
     $user = $this->User->find('first', array('conditions' => array('User.seo_username' => $seo_username)));
-	
+	$userid=$user['User']['id'];
+	$this->usergame_user_panel2($userid);
 	if($user==NULL)
 	$this->redirect('/');
 	
-	$userid=$user['User']['id'];
+	
     $userName = $user['User']['username'];
 	$limit=12;
 	$cond= $this->Game->find('all', array('conditions' => array('Game.active'=>'1','Game.user_id'=>$userid),'limit' => $limit,'order' => array('Game.recommend' => 'desc'
