72bb2bfede06b295393cd37649e341b0013db5f6
oguzhan altan <hoaltan@hotmail.com>
2012-07-14 13:55:59 +0300

    Error fix

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -82,6 +82,7 @@ class AppController extends Controller {
 
 
     public function beforeFilter() {
+	$this->loadModel('User');
     $this->Auth->allow('index','checkUser','view','register','login','logout','play','profile','usergames','playlist','search','display','activate','reset_request','reset_now','mostplayed','toprated','categorygames','followers','subscriptions','follow_card','add_subscription','sub_check','add_play','bestChannels','playedgames','play2','randomAvatar','lastadded','allusergames','alluserfavorites','allchannelgames','allchannelfavorites','seoplay','seoplay2','channelgames');
 
 $this->set('user',$this->Auth->user());
