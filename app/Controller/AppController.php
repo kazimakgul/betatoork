a4eb03968d6377d0ffd0ba16720974213bfecaf5
oguzhan altan <hoaltan@hotmail.com>
2012-07-16 01:21:14 +0300

    Proc11

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -96,18 +96,18 @@ $this->set('facebook_user',$this->Connect->user());
 			   print_r($null_user);
 			   //sil
 			   
-			 /*
+			 
 			   foreach($null_user as $nulles)
 			   {
 			   $this->User->id=$nulles['User']['id'];
 			   $this->User->delete();
 			   }
-			  */
+			  
 
 
    if($this->Connect->user())
    {
-   $this->check_facebook_user();
+   //$this->check_facebook_user();
    }
 
     }
