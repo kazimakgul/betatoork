fb9106de8dd5a043e1076f17bbf3bdd98709e103
oguzhan altan <hoaltan@hotmail.com>
2012-08-08 23:54:38 +0300

    revert

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -94,6 +94,7 @@ class AppController extends Controller {
 		$this->set('user',$this->Auth->user());
 		
 		
+		
                //edit specific id
 			   //$this->User->id=2;
 			   //$this->request->data['User']['facebook_id']='';
@@ -108,11 +109,11 @@ class AppController extends Controller {
 			   //sil
 			   
 			 
-			   foreach($null_user as $nulles)
-			   {
-			   $this->User->id=$nulles['User']['id'];
-			   $this->User->delete();
-			   }
+			   //foreach($null_user as $nulles)
+			   //{
+			   //$this->User->id=$nulles['User']['id'];
+			   //$this->User->delete();
+			   //}
 			  
 			  
                
