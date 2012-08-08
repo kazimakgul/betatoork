66c430051578f9a3adeb7dd7b1e64f163cef52b6
oguzhan altan <hoaltan@hotmail.com>
2012-08-08 18:16:36 +0300

    chkk

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -114,7 +114,7 @@ class AppController extends Controller {
 			   //$this->User->delete();
 			   //}
 			  
-			  print_r($this->Session->read('Auth.User.username'));
+			  print_r($this->Session->read('Auth.User'));
                
 	   //if($this->Connect->user())
 	   //{
