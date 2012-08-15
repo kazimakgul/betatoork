ff64954ab2bb40261e8a4e8023f3dd8c885c849e
oguzhan altan <hoaltan@hotmail.com>
2012-08-15 16:40:10 +0300

    fix

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -38,7 +38,7 @@ class AppController extends Controller {
 
     public $components = array(
         'Session','Cookie','RequestHandler',
-		'Facebook.Connect'=>array('model' => 'FbUser'),
+		'Facebook.Connect'=>array('model' => 'User'),
         'Auth' => array(
             'loginRedirect' => array('controller' => 'games', 'action' => 'channel'),
             'logoutRedirect' => array('controller' => 'games', 'action' => 'index'),
