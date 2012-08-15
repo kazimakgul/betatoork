cd2a20fd94c0284746efda9fa09dfc68e4a2ad9c
oguzhan altan <hoaltan@hotmail.com>
2012-08-15 16:39:22 +0300

    delete all fb ids

--- app/Plugin/Facebook/Controller/Component/ConnectComponent.php
+++ app/Plugin/Facebook/Controller/Component/ConnectComponent.php
@@ -155,8 +155,12 @@ class ConnectComponent extends Component {
 			}
 			//create the user if we don't have one
 			elseif(empty($this->authUser) && $this->createUser) {
+			   
+			  if($this->Auth->user('facebook_id')!=NULL)
+			  {
 				$this->authUser[$this->User->alias]['facebook_id'] = $this->uid;
 				$this->authUser[$this->User->alias][$this->modelFields['password']] = $Auth->password(FacebookInfo::randPass());
+			  }	
 				if($this->__runCallback('beforeFacebookSave')){
 					$this->hasAccount = ($this->User->save($this->authUser, array('validate' => false)));
 				}
