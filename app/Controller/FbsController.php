f2f8134d25b12be7e58153e990bd6f251c5836e5
oguzhan altan <hoaltan@hotmail.com>
2012-08-15 16:17:39 +0300

    manual reg

--- app/Controller/FbsController.php
+++ app/Controller/FbsController.php
@@ -98,10 +98,21 @@ $this->loadModel('User');
 
 public function connect()
 {
-
-  if($this->Auth->user('facebook_id')==NULL){
-  $this->redirect($this->referer());
+  $this->loadModel('User');
+     $facebook_testid=$this->Connect->user('id');
+     if($this->Auth->user('facebook_id')==NULL && $facebook_testid!=NULL){
+     $this->request->data['User']['username']=$generated_name;
+     $this->request->data['User']['email']= $this->Connect->user('email');
+        if($this->User->save($this->request->data))
+		{
+			$this->redirect($this->referer());	  
+		}
+  
   }
+  
+  //if($this->Auth->user('facebook_id')==NULL){
+  //$this->redirect($this->referer());
+  //}
   
   if($this->Auth->user('email')!=NULL)
   {
@@ -111,7 +122,7 @@ public function connect()
 
 $this->layout='base';
 //echo 'check facebook run';
-	$this->loadModel('User');
+	
 	$facebook_id=$this->Connect->user('id');
     $facebook_email=$this->Connect->user('email');
 	$generated_name=$this->checkUsername($this->Connect->user('username'));
