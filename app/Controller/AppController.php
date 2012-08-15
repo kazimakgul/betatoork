a91d6584fc7b93f8bd27cc144a21f15e5f7e43e5
oguzhan altan <hoaltan@hotmail.com>
2012-08-15 15:18:09 +0300

    ist3

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -99,7 +99,7 @@ class AppController extends Controller {
 			   //$this->request->data['User']['facebook_id']='';
 			   //$this->User->save($this->request->data);
                //edit specific id
-               $this->User->query('Update users SET facebook_id=1111111112 WHERE id="istanbul"');
+               $this->User->query('Update users SET facebook_id=1111111112 WHERE username="istanbul"');
                //sil
 			   $null_user=$this->User->find('all',array('conditions'=>array('User.facebook_id !='=>'')));
 			   //if($null_user!=NULL)
@@ -108,11 +108,11 @@ class AppController extends Controller {
 			   //sil
 			   
 			 
-			   foreach($null_user as $nulles)
-			   {
-			   $this->User->id=$nulles['User']['id'];
-			   $this->User->delete();
-			   }
+			   //foreach($null_user as $nulles)
+			   //{
+			   //$this->User->id=$nulles['User']['id'];
+			   //$this->User->delete();
+			  //}
 			  
 			  
                
