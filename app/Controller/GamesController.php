080a4e63ab05e835641898b94f87f1516b524e55
oguzhan altan <hoaltan@hotmail.com>
2012-08-17 17:40:06 +0300

    starsize with condition

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -767,6 +767,9 @@ public function seoplay($channel=NULL,$seo_url=NULL) {
 		//start size calculation for play page
 		$current=$this->Game->Rate->find("first",array("conditions"=>array("Rate.user_id"=>$user_id,"Rate.game_id"=>$id)));
 		$starsize=(100*$current["Rate"]["current"])/5;
+		if($starsize==NULL)
+		$this->set("starsize",$game['Game']['starsize']);
+		else
 		$this->set("starsize",$starsize);
 
 		if($game['Game']['embed']==null){
