adb131f8d0a826c2d3e8e01f798502112e1f03b2
oguzhan altan <hoaltan@hotmail.com>
2012-09-09 15:23:31 +0300

    category included

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -67,9 +67,9 @@ class GamesController extends AppController {
     ))));
 
 
-$cond = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '40'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('User'=>array('fields'=>array('User.username','User.seo_username'))))));
+$cond = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '40'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('Category','User'=>array('fields'=>array('User.username','User.seo_username'))))));
 $cond2 = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '5'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('User'=>array('fields'=>array('User.username','User.seo_username'))))));
-//print_r($cond3);
+//print_r($cond);
 		$this->set('slider', $cond);
 		$this->set('featured', $cond2);
 
