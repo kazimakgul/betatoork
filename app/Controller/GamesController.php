80fd5b20594857db21e2ad228bb384ce67a7dd4c
oguzhan altan <hoaltan@hotmail.com>
2012-09-09 16:31:41 +0300

    comment out

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -69,7 +69,7 @@ class GamesController extends AppController {
 
 $cond = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '40'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('fields'=>array('Game.name,Game.seo_url,Game.id,Game.picture,Game.starsize'),'Category','User'=>array('fields'=>array('User.username','User.seo_username'))))));
 $cond2 = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '5'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('fields'=>array('Game.name,Game.seo_url,Game.id,Game.picture,Game.starsize'),'User'=>array('fields'=>array('User.username','User.seo_username'))))));
-print_r($cond);
+//print_r($cond);
 		$this->set('slider', $cond);
 		$this->set('featured', $cond2);
 
