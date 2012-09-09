605d81555d655eaffd86b63a3e6d1bede56ef748
oguzhan altan <hoaltan@hotmail.com>
2012-09-09 15:16:20 +0300

    for slider

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -66,10 +66,9 @@ class GamesController extends AppController {
 		$this->set('most_played_games', $this->Game->find('all', array('conditions' => array('Game.active'=>'1'),'limit' => $limit,'order' => array('Game.playcount' => 'desc'
     ))));
 
-$cond = $this->Favorite->find('all', array('conditions' => array('Favorite.active'=>'1','Favorite.user_id' => '40'),'limit' => $limit,'order' => array('Favorite.recommend' => 'desc'),'recursive' => 0));
-$cond2 = $this->Favorite->find('all', array('conditions' => array('Favorite.active'=>'1','Favorite.user_id' => '5'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'recursive' => 0));
-//$cond3 = $this->Favorite->contain(array('Game.name="Gravity Guy"'));
-$cond3 = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '5'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('User'=>array('fields'=>array('User.username','User.seo_username'))))));
+
+$cond = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '40'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('User'=>array('fields'=>array('User.username','User.seo_username'))))));
+$cond2 = $this->Favorite->find('all',array('conditions'=>array('Favorite.active'=>1,'Favorite.user_id' => '5'),'limit' =>$limit,'order' => array('Favorite.recommend' => 'desc'),'contain'=>array('Game'=>array('User'=>array('fields'=>array('User.username','User.seo_username'))))));
 //print_r($cond3);
 		$this->set('slider', $cond);
 		$this->set('featured', $cond2);
