1f15ac502b2f528cce6fec4b1ac7154db9122345
oguzhan altan <hoaltan@hotmail.com>
2012-09-10 03:19:46 +0300

    played games

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -71,6 +71,7 @@ class AppController extends Controller {
             ), 
         ),
         'Playcount' => array(
+		'contain'=>array('Game'=>array('fields'=>array('Game.name,Game.seo_url,Game.id,Game.picture,Game.starsize'),'User'=>array('fields'=>array('User.username','User.seo_username')))),
             'limit' => 28,
             'order' => array(
                 'Game.recommend' => 'desc',
