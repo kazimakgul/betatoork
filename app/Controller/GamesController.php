56cd34eb28b0574f86871c77ac22d210ec7428c4
oguzhan altan <hoaltan@hotmail.com>
2012-09-13 18:30:01 +0300

    comment out

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -845,7 +845,7 @@ public function seoplay($channel=NULL,$seo_url=NULL) {
 		
 		//ReCoded
 		$game = $this->Game->find('first', array('conditions' => array('Game.seo_url'=>$seo_url),'fields'=>array('User.username,User.seo_username,Game.name,Game.user_id,Game.link,Game.starsize,Game.embed,Game.description,Game.id,Game.active,Game.picture'),'contain'=>array('User'=>array('fields'=>array('User.username,User.seo_username,User.adcode'),'conditions'=>array('User.seo_username'=>$channel)))));
-		print_r($game);
+
 		if($game!=NULL)
 		$id=$game['Game']['id'];
 		$this->fav_check($id);
