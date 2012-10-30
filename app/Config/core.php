24afd5a7035ced6af94fede5f49a2ec0ed282f1c
oguzhan altan <hoaltan@hotmail.com>
2012-10-30 21:38:02 +0200

    general revert

--- app/Config/core.php
+++ app/Config/core.php
@@ -240,7 +240,7 @@ Configure::write('S3.name','betatoorkpics');
 # toork S3 path -> https://s3.amazonaws.com/betatoorkpics
 
 # Best Channel Settings
-$multiple=array(
+/*$multiple=array(
 'playcount'=>3,
 'subscribe'=>1,
 'subscribeto'=>10,
@@ -248,7 +248,7 @@ $multiple=array(
 'plainrates'=>1
 );
 Configure::write('multiples',$multiple);
-
+*/
 
 /**
  * Pick the caching engine to use.  If APC is enabled use it.
