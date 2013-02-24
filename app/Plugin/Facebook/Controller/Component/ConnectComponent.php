2725454a13a0df126b57740678db58bb02c8b27d
oguzhanaltan <hoaltan@hotmail.com>
2013-02-25 01:11:26 +0200

    comm out

--- app/Plugin/Facebook/Controller/Component/ConnectComponent.php
+++ app/Plugin/Facebook/Controller/Component/ConnectComponent.php
@@ -162,16 +162,16 @@ class ConnectComponent extends Component {
 			elseif(empty($this->authUser) && $this->createUser) {echo 'we have to create';
 				$this->authUser[$this->User->alias]['facebook_id'] = $this->uid;
                 $this->authUser[$this->User->alias][$this->modelFields['password']] = $Auth->password(FacebookInfo::randPass());
+				/*
 				if($this->__runCallback('beforeFacebookSave')){
 					$this->hasAccount = ($this->User->save($this->authUser, array('validate' => false)));
-					echo 'thats it';
-				}	
+				}	*/
 				else {
 					$this->authUser = null;
 				}
 			}
 			//Login user if we have one
-			if($this->authUser){echo 'Login user';
+			if($this->authUser){
 				$this->__runCallback('beforeFacebookLogin', $this->authUser);
 				$Auth->authenticate = array(
 					'Form' => array(
