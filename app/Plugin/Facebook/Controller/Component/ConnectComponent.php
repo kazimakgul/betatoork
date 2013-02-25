df8d47957392578fd9320729e9fe7d912e11c8e0
oguzhanaltan <hoaltan@hotmail.com>
2013-02-25 05:12:10 +0200

    again

--- app/Plugin/Facebook/Controller/Component/ConnectComponent.php
+++ app/Plugin/Facebook/Controller/Component/ConnectComponent.php
@@ -178,7 +178,7 @@ class ConnectComponent extends Component {
 				
 				
 				if($this->__runCallback('beforeFacebookSave')){
-					$this->hasAccount = $Controller->User->save($Controller->request->data, array('validate' => false));
+					$this->hasAccount = $Controller->User->save($Controller->request->data);
 				}	
 				else {
 					$this->authUser = null;
