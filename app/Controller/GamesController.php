dcadc3f7f4480a533369e3ad0bb7ab12dd10eff2
oguzhanaltan <hoaltan@hotmail.com>
2013-06-17 03:08:01 +0300

    url

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -1905,12 +1905,11 @@ echo '<a href="'.$image['src'].'"><img width="130px" src="'.$image['src'].'"></a
   
   if(isset($url) && $url!=NULL)
   {
-      $url="http://www.toork.com";
-      $command = "xvfb-run --server-args='-screen 0, 1024x768x24' wkhtmltopdf ".$url." /var/www/betatoork/app/webroot/upload/file2aa9.pdf";
+      $command = "xvfb-run --server-args='-screen 0, 1024x768x24' wkhtmltopdf ".$url." /var/www/betatoork/app/webroot/upload/file2aa10.pdf";
       exec($command, $output, $ret);
 	  print_r($output);print_r($ret);
       if ($ret) {
-      $command2 = "convert /var/www/betatoork/app/webroot/upload/file2aa9.pdf -append /var/www/betatoork/app/webroot/upload/file2aa9.png";
+      $command2 = "convert /var/www/betatoork/app/webroot/upload/file2aa10.pdf -append /var/www/betatoork/app/webroot/upload/file2aa10.png";
       exec($command2, $output2, $ret2);
       die;
                 }
