9fa089620876904c40504d40db6411830b5a0f2c
oguzhanaltan <hoaltan@hotmail.com>
2013-06-26 01:26:44 +0300

    check again

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -1905,11 +1905,11 @@ echo '<a href="'.$image['src'].'"><img width="130px" src="'.$image['src'].'"></a
    App::uses('Folder', 'Utility');
    App::uses('File', 'Utility');
       
-	  $dir = new Folder(WWW_ROOT."/../../");
+	  $dir = new Folder(WWW_ROOT."/../../../");
 		    $files = $dir->find('.*');
 			print_r($files);echo 'checked';
 			
-			break;
+			break;echo 'after break';
 	  
 	  if($userid = $this->Session->read('Auth.User.id'))
       {
