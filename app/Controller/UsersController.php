f7b3425fe61186bb01123e51d4040d05efd3e975
oguzhanaltan <hoaltan@hotmail.com>
2013-07-14 03:13:24 +0300

    Revert "fix"
    This reverts commit 082a9346d56dd6e9bfb5f5b8e826250c6127c7cf.

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -580,8 +580,8 @@ public function set_suggested_channels()
 			$this->request->data["User"]["picture"]=$this->request->data["User"]["banner"];
 			//$this->request->data["User"]["banner"]=$this->request->data["User"]["banner"]["name"];
 			
-			$noextension=rtrim($this->request->data["User"]["banner"]["name"], '.'.$this->getExtension($this->request->data["User"]["banner"]["name"]));
-			$yesextension=$noextension.'_original.'.$this->getExtension($this->request->data["User"]["banner"]["name"]);
+			$noextension=rtrim($this->request->data["User"]["banner"]["name"], $this->getExtension($this->request->data["User"]["banner"]["name"]));
+			$yesextension=$noextension.'_original'.$this->getExtension($this->request->data["User"]["banner"]["name"]);
 			$this->request->data["User"]["banner"]=$yesextension;
 			
 			}
