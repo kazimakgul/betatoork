fa835c8500091290ec25f3946d80d5245a62a5e8
oguzhanaltan <hoaltan@hotmail.com>
2013-07-21 17:49:26 +0300

    duplicate of

--- app/Controller/ApisController.php
+++ app/Controller/ApisController.php
@@ -157,7 +157,7 @@ class ApisController extends AppController {
 	  $this->request->data['Game']['picture'] = $fileName.".png";
 	 		
 		//seourl begins
-		$this->request->data['Game']['seo_url']=$this->duplicate_seourl(strtolower(str_replace(' ','-',$this->request->data['Game']['name'])));
+		$this->request->data['Game']['seo_url']=str_replace('_','',Inflector::slug(strtolower(str_replace(' ','-',$this->request->data['Game']['name']))),'_');
 		//seourl ends
 			
 			$this->Game->create();
