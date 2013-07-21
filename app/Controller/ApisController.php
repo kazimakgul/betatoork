7d4c3b0f21df116d22d10181ca32ef053a02f671
oguzhanaltan <hoaltan@hotmail.com>
2013-07-21 19:01:35 +0300

    checkDuplicateSeoUrl

--- app/Controller/ApisController.php
+++ app/Controller/ApisController.php
@@ -184,7 +184,7 @@ return $material.$number;
 	  $this->request->data['Game']['picture'] = $fileName.".png";
 	 		
 		//seourl begins
-		$this->request->data['Game']['seo_url']=$this->duplicate_seourl(str_replace('_','',Inflector::slug(strtolower($this->request->data['Game']['name']))));
+		$this->request->data['Game']['seo_url']=$this->checkDuplicateSeoUrl(str_replace('_','',Inflector::slug(strtolower($this->request->data['Game']['name']))));
 		//seourl ends
 			
 			$this->Game->create();
