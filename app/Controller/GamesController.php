f45784bf55320547b0e66ced1500090d02d54458
oguzhanaltan <hoaltan@hotmail.com>
2013-11-30 23:11:38 +0200

    teste with 6

--- app/Controller/GamesController.php
+++ app/Controller/GamesController.php
@@ -2057,7 +2057,7 @@ public function seoplay($channel=NULL,$seo_url=NULL) {
 
 	public function headerlogin() {
 		$userid = $this->Session->read('Auth.User.id');
-	   	$user = $this->User->find('first', array('conditions'=> array('User.id'=>$userid)));
+	   	$user = $this->User->find('first', array('conditions'=> array('User.id'=>6)));
 	   	$userName = $user['User']['username'];
 
 	    $this->set('user',$user);
