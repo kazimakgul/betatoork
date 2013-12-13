44e18d9991e69459f7bf0d4d750f4c079e338d7f
oguzhanaltan <hoaltan@hotmail.com>
2013-12-13 21:37:45 +0200

    mentionlowercase

--- vendors/wallscript/tolink.php
+++ vendors/wallscript/tolink.php
@@ -10,9 +10,19 @@ function tolink($text,$root=NULL){
         '\\1<a href="http://\\2" target="_blank" rel="nofollow">\\2</a>', $text);
         $text = eregi_replace('([_\.0-9a-z-]+@([0-9a-z][0-9a-z-]+\.)+[a-z]{2,4})',
         '<a href="mailto:\\1"  rel="nofollow">\\1</a>', $text);
+		$text=fixOrdinals($text);
 		$text = preg_replace('#@([\\d\\w]+)#', '<a class="btn-link" href="'.$root.'$1">$0</a>', $text);
 		$text = preg_replace('/#([\\d\\w]+)/', '<a class="btn-link" href="'.$root.'games/hashtag/$1">$0</a>', $text);
         return $text;
+}
+
+function fixOrdinals($string) { 
+    return preg_replace_callback("#@([\\d\\w]+)#",
+        function ($matches) { 
+            return strtolower($matches[0]);
+        },
+        $string
+    );
 }
 
 ?>
