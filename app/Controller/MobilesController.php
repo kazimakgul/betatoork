60e06b4b6adf3081f8b1857a1a6cf99222c527c2
emircanok <emircan@toork.com>
2014-05-23 17:03:34 +0300

    MObile

--- app/Controller/MobilesController.php
+++ app/Controller/MobilesController.php
@@ -1,5 +1,7 @@
 <?php
+
 App::uses('AppController', 'Controller');
+
 /**
  * Business Controller
  *
@@ -7,72 +9,108 @@ App::uses('AppController', 'Controller');
  */
 class MobilesController extends AppController {
 
-	public $name = 'Mobiles';
-	var $uses = array('Mobiles','Game','User','Favorite','Subscription','Playcount','Rate','Userstat','Gamestat','Category','Activity','Cloneship','CakeEmail', 'Network/Email');
-    public $helpers = array('Html', 'Form','Upload','Recaptcha.Recaptcha','Time');
-    public $components = array('Amazonsdk.Amazon','Recaptcha.Recaptcha','Common');
-    
+    public $name = 'Mobiles';
+    var $uses = array('Mobiles', 'Game', 'User', 'Favorite', 'Subscription', 'Playcount', 'Rate', 'Userstat', 'Gamestat', 'Category', 'Activity', 'Cloneship', 'CakeEmail', 'Network/Email');
+    public $helpers = array('Html', 'Form', 'Upload', 'Recaptcha.Recaptcha', 'Time');
+    public $components = array('Amazonsdk.Amazon', 'Recaptcha.Recaptcha', 'Common');
+
     //=====Kullanici sisteme login ise=======
- 	public function isAuthorized($user) {
-	    if (parent::isAuthorized($user)) {
-	        return true;
-	    }
+    public function isAuthorized($user) {
+        if (parent::isAuthorized($user)) {
+            return true;
+        }
 
-	    if (($this->action === 'play') ||  ($this->action === 'index')) {
-	       // All registered users can add posts
-	        return true;
-	    }
-	    if (in_array($this->action, array('edit2','delete'))) {
-	        $gameId = $this->request->params['pass'][0];
-	        return $this->Game->isOwnedBy($gameId, $user['id']);
-	    }
+        if (($this->action === 'play') || ($this->action === 'index')) {
+            // All registered users can add posts
+            return true;
+        }
+        if (in_array($this->action, array('edit2', 'delete'))) {
+            $gameId = $this->request->params['pass'][0];
+            return $this->Game->isOwnedBy($gameId, $user['id']);
+        }
 
-	    return false;
-	}
-     
-	public function afterFilter() {
-	
-	App::uses('Folder', 'Utility');
-    App::uses('File', 'Utility');
-	
-	//===Eger upload adinda bir klasör varsa siler.====
-        $upload_dir = new Folder(WWW_ROOT ."upload");
- 	    $updir=$upload_dir->pwd();
-		if($updir!=NULL)
-		{
-		$upload_dir->delete();
-		//print_r($upload_dir->errors());
-		}
-    //===//Eger upload adinda bir klasör varsa siler.====
-	}
-	
-	
+        return false;
+    }
 
+    public function afterFilter() {
 
+        App::uses('Folder', 'Utility');
+        App::uses('File', 'Utility');
 
-	
-	public function index($userid) {
+        //===Eger upload adinda bir klasör varsa siler.====
+        $upload_dir = new Folder(WWW_ROOT . "upload");
+        $updir = $upload_dir->pwd();
+        if ($updir != NULL) {
+            $upload_dir->delete();
+            //print_r($upload_dir->errors());
+        }
+        //===//Eger upload adinda bir klasör varsa siler.====
+    }
 
-		$this->layout = 'Mobile/mobile';
+    public function index($userid) {
 
-		
-		$this->set('title_for_layout', 'Clone Games');
-		$this->set('description_for_layout', 'Discover collect and share games. Clone games and create your own game channel.');
-		$this->set('author_for_layout', 'Clone');
-	}
-	
-	public function play($id=NULL) {
-	//Getting Random Game Data
+        /**
+         * Kullanılan Layout
+         */
+        $this->layout = 'Mobile/mobile';
 
-		$this->layout='Mobile/mobile';
+        /**
+         * Layout Değişkenleri
+         */
+        $this->set('title_for_layout', 'Clone Games');
+        $this->set('description_for_layout', 'Discover collect and share games. Clone games and create your own game channel.');
+        $this->set('author_for_layout', 'Clone');
 
-		$this->set('title_for_layout', 'Clone Games');
-		$this->set('description_for_layout', 'Discover collect and share games. Clone games and create your own game channel.');
-		$this->set('author_for_layout', 'Clone');
+        /**
+         * Kullanıcı Bilgileri
+         */
+        $user = $this->User->find('first', array(
+            'conditions' => array(
+                'User.id' => $userid
+            ),
+            'fields' => array(
+                '*'
+            )
+                )
+        );
 
-	}	
-	
+        //  print_r($user);
+        //  exit;
 
+        $PaginateLimit = 6;
+        $this->paginate = array(
+            'Game' => array(
+                'conditions' => array(
+                    'Game.active' => '1',
+                    'Game.user_id' => $userid
+                ),
+                'limit' => $PaginateLimit,
+                'order' => array(
+                    'Game.recommend' => 'desc'
+                ),
+                'contain' => array(
+                    'Gamestat' => array(
+                        'fields' => array(
+                            'Gamestat.playcount,Gamestat.favcount,Gamestat.totalclone'
+                        )
+                    )
+                )
+            )
+        );
+        $cond = $this->paginate('Game');
+        $this->set('games', $cond);
 
-	
+        //  $keys = $this->Game->query("SELECT * FROM games as Game WHERE user_id=$userid and description like '%" . $param . "%' or name like '%" . $param . "%'");
+    }
+
+    public function play($id = NULL) {
+        //Getting Random Game Data
+
+        $this->layout = 'Mobile/mobile';
+
+        $this->set('title_for_layout', 'Clone Games');
+        $this->set('description_for_layout', 'Discover collect and share games. Clone games and create your own game channel.');
+        $this->set('author_for_layout', 'Clone');
+    }
+
 }
--- app/View/Elements/mobile/gamebox.ctp
+++ app/View/Elements/mobile/gamebox.ctp
@@ -1,12 +1,42 @@
-<?php $play=$this->Html->url(array("controller" => "mobiles","action" =>"play",2)); ?>
-
-<div class="col-sm-4">
-      <div class="thumbnail">
-        <img style="margin:0px; padding:0px;" data-src="holder.js/300x200" alt="300x200" src="http://www.edweek.org/media/2013/01/22/18games_birds600.jpg" style="width: 300px; height: 200px;">
-        <div class="caption">
-          <h3>Angry Birds Starwars</h3>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-          <p><a href="<?php echo $play;?>" class="btn btn-primary" role="button">Play</a> </p>
+<?php
+$play = $this->Html->url(array("controller" => "mobiles", "action" => "play", 2));
+foreach ($games as $game) {
+    ?>
+    <div class="col-sm-4">
+        <div class="thumbnail">
+            <!-- <img style="margin:0px; padding:0px;" data-src="holder.js/300x200" alt="300x200" src="http://www.edweek.org/media/2013/01/22/18games_birds600.jpg" style="width: 300px; height: 200px;"> -->
+            <?php echo $this->Upload->image($game, 'Game.picture', array('style' => 'toorksize'), array('style' => 'toorksize', 'class' => 'panel-image-preview', 'alt' => $game['Game']['name'], 'onerror' => 'imgError(this,"toorksize");', 'width' => '100%')); ?>
+            <div class="caption">
+                <h3>Angry Birds Starwars</h3>
+                <!--
+                <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+                <p><a href="<?php echo $play; ?>" class="btn btn-primary" role="button">Play</a> </p>
+                -->
+                <div class="row">
+                    <span class="col-md-6" style='margin-left:10px;'>
+                        <div class= 'centerrate2'>
+                            <div class="stars2"  data-toggle="tooltip" data-original-title="<?= $game['Game']['rate_count']; ?> Rates">
+                                <div class="ratingbar2" style="width: <?php echo $game['Game']['starsize']; ?>%;"></div>
+                                <div class="star2">
+                                    <div class="star2">
+                                        <div class="star2">
+                                            <div class="star2">
+                                                <div class="star2">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </span>
+                    <span class="col-md-5">
+                        <i data-toggle="tooltip" title="<?php echo $game['Gamestat']['playcount']; ?> Plays" class="fa fa-play green"></i>
+                        <i data-toggle="tooltip" title="<?php echo $game['Gamestat']['favcount']; ?> Favorites" class="fa fa-heart red"></i>
+                        <i data-toggle="tooltip" title="<?php echo $game['Gamestat']['totalclone']; ?> Clones" class="fa fa-plus-square darkblue"></i>
+                    </span>
+                </div>
+            </div>
         </div>
-      </div>
-</div>
+    </div>
+<?php } ?>
