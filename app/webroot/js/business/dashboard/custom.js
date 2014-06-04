2f4bdd5eafdfb7de5650e3afb6436f6e11d106d2
emircanok <emircan@toork.com>
2014-06-04 12:27:30 +0300

    Javascript Düzenleme

--- app/webroot/js/business/dashboard/custom.js
+++ app/webroot/js/business/dashboard/custom.js
@@ -1,41 +1,95 @@
 $(document).ready(function() {
 
-			// Datepicker
-	        $('.datepicker').datepicker({
-	        	autoclose: true,
-	        	orientation: 'left bottom',
-	        });
+    // Datepicker
+    $('.datepicker').datepicker({
+        autoclose: true,
+        orientation: 'left bottom',
+    });
 
-	/*
-	 *	Update Form Post Method
-	 * 	@param #attr.val(), link => Update controller
-	 *	@return data.error=> error.id or data.success=> success.id
-	 */
-	$('#updateButton').click(function () {
-		var link = updateData; //Businesses updatedata function run.
-		var attr = $('#attr').val(); //Form control value
+    /*
+     *	Update Form Post Method
+     * 	@param #attr.val(), link => Update controller
+     *	@return data.error=> error.id or data.success=> success.id
+     */
+    $('#updateButton').click(function() {
+        var link = updateData; //Businesses updatedata function run.
+        var attr = $('#attr').val(); //Form control value
 
-		if(attr == "profile_update")
-		{
-			$.post(link, {
-						attr	: $('#attr').val(),
-						time	: $('#user_time_zone').val(),
-						strt	: $('#street').val(),
-						cont	: $('#country').val(),
-						zip		: $('#zip').val(),
-						pass	: $('#pass').val()
-					},
-					function (data) {
-						if (data.error) {alert(data.error); } // error.id ye göre mesaj yazdırcak..
-					}, 'json');
-		}
-		else if(attr == "channel_update"){
-			//Liste bekleniyor.
-			
-		}else
-		{
-			
-		}
-	});
+        if (attr == "profile_update")
+        {
+            $.post(link, {
+                attr: $('#attr').val(),
+                time: $('#user_time_zone').val(),
+                strt: $('#street').val(),
+                cont: $('#country').val(),
+                zip: $('#zip').val(),
+                pass: $('#pass').val()
+            },
+            function(data) {
+                if (data.error) {
+                    alert(data.error);
+                } // error.id ye göre mesaj yazdırcak..
+            }, 'json');
+        }
+        else if (attr == "channel_update") {
+            //Liste bekleniyor.
+
+        } else
+        {
+
+        }
+    });
+
+
+    /**
+     * 
+     *  MY GAMES
+     *
+     */
+
+
+    // User list checkboxes
+    var $allUsers = $(".select-users input:checkbox");
+    var $checkboxes = $("[name='select-user']");
+    $allUsers.change(function() {
+        var checked = $allUsers.is(":checked");
+        if (checked) {
+            $checkboxes.prop("checked", "checked");
+            toggleBulkActions(checked, $checkboxes.length);
+        } else {
+            $checkboxes.prop("checked", "");
+            toggleBulkActions(checked, 0);
+        }
+    });
+    $checkboxes.change(function() {
+        var anyChecked = $(".user [name='select-user']:checked");
+        toggleBulkActions(anyChecked.length, anyChecked.length);
+    });
+    function toggleBulkActions(shouldShow, checkedCount) {
+        if (shouldShow) {
+            $(".users-list .header").hide();
+            $(".users-list .header.select-users").addClass("active").find(".total-checked").html("(" + checkedCount + " total games)");
+
+        } else {
+            $(".users-list .header").show();
+            $(".users-list .header.select-users").removeClass("active");
+        }
+    }
+    // Grid switcher
+    $btns = $(".grid-view");
+    $views = $(".users-view");
+    $btns.click(function(e) {
+        e.preventDefault();
+        $btns.removeClass("active");
+        $(this).addClass("active");
+
+        $views.removeClass("active");
+
+        $(".users-grid").hide();
+        $(".users-list").hide();
+
+        $($(this).data("grid")).show();
+    });
+
 });
 
