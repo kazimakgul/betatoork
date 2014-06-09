6fd86f56527c724bce8df5082f275a3d254a6990
oguzhanaltan <hoaltan@hotmail.com>
2014-06-09 17:42:04 +0300

    *****Custom js for dashboard added*****

--- app/webroot/js/business/dashboard/custom.js
+++ app/webroot/js/business/dashboard/custom.js
@@ -96,7 +96,46 @@ $(document).ready(function() {
 
 
 
+   //Controller functions for modals of avatar begins
+$('#avatarframe').load(function(){
+  $(this).contents().find("#close_panel").on('click', function(event) { $('#pictureChange').modal('toggle'); });
+});
+
+$('#avatarframe').load(function(){
+  $(this).contents().find("#set_photo").on('click', function(event) { 
+   $('#pictureChange').modal('toggle');
+   $('#channel_avatar').attr('src','http://www.imageyourself.net/images/website/loading.gif');
    
+   setTimeout(function(){
+        var new_img = $('iframe[id=avatarframe]').contents().find('#new_image_link').val();
+        $('#channel_avatar').attr('src',new_img);              
+   },1000);
+
+   });
+
+//var name = $('iframe[id=avatarframe]').contents().find('#selected_image').val();
+//alert(name);
+});
+//Controller functions for modals of avatar ends
+
+//Controller functions for modals of cover begins
+$('#coverframe').load(function(){
+  $(this).contents().find("#close_panel").on('click', function(event) { $('#coverChange').modal('toggle'); });
+});
+
+$('#coverframe').load(function(){
+  $(this).contents().find("#set_photo").on('click', function(event) { 
+   $('#coverChange').modal('toggle');
+   $('#user_cover').css('background-image','url(http://3.bp.blogspot.com/-13dC5LhMbMM/T6NpcCU7obI/AAAAAAAAAVE/kt0XhVIV_zU/s200/loading.gif)');  
+   setTimeout(function(){
+        var new_img = $('iframe[id=coverframe]').contents().find('#new_image_link').val();
+        $('#user_cover').css('background-image','url('+new_img+')');           
+   },1000);
+
+   });
+
+});
+//Controller functions for modals of covers ends
 
 
 
