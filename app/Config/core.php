a9e9448d8b0045e7a3fbffef540181a8fac52c9b
Kazim Akgul <kazimakgul@KazimAkgul.local>
2014-06-21 12:13:35 +0300

    S3 fix

--- app/Config/core.php
+++ app/Config/core.php
@@ -231,10 +231,10 @@
  */
 	//date_default_timezone_set('UTC');
 
-#Configure::write('S3.url','https://s3.amazonaws.com/betatoorkpics');
-#Configure::write('S3.name','betatoorkpics');
-Configure::write('S3.name','betatoorkpicstest');
-Configure::write('S3.url','https://s3.amazonaws.com/betatoorkpicstest');
+Configure::write('S3.url','https://s3.amazonaws.com/betatoorkpics');
+Configure::write('S3.name','betatoorkpics');
+#Configure::write('S3.name','betatoorkpicstest');
+#Configure::write('S3.url','https://s3.amazonaws.com/betatoorkpicstest');
 
 Configure::write('S3-games.name','games.clone.gs');
 Configure::write('S3-games.url','http://games.clone.gs');
