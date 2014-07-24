4eaba8bfb1fe0bf8f50d6d985e1e83c74fcd7e20
oguzhanaltan <hoaltan@hotmail.com>
2014-07-25 01:34:29 +0300

    wow

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -140,7 +140,7 @@ if(strlen($subdomain)>0 && !in_array($subdomain,array('www'))){
 if(checkdnsrr(env("HTTP_HOST"), "CNAME"))
 {
            session_set_cookie_params(0, '/', '.nishgame.com'); 
-           session_start();
+          
 }else{
 	ini_set('session.cookie_domain', '.clone.gs');
 }
