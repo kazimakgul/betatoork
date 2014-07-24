95fae685844463ed4ed9c43593f7d5829daf7d9b
oguzhanaltan <hoaltan@hotmail.com>
2014-07-25 00:55:31 +0300

    nishgame.nishgame.com

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -130,11 +130,9 @@ if(strlen($subdomain)>0 && !in_array($subdomain,array('www'))){
 
 
 //Bu condition tamamen kaldırılacak.Local de de login olabilmemizi sağlıyor.
-//if($_SERVER['HTTP_HOST']!="127.0.0.1" && $_SERVER['HTTP_HOST']!="localhost") {
-//ini_set('session.cookie_domain', '.clone.gs');
-//}
-session_set_cookie_params(0, '/', 'nishgame.nishgame.com'); 
-session_start();
+if($_SERVER['HTTP_HOST']!="127.0.0.1" && $_SERVER['HTTP_HOST']!="localhost") {
+ini_set('session.cookie_domain', 'nishgame.nishgame.com');
+}
 
 
 
