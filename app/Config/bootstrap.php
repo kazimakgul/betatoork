32c2b93f06702a0d15ed99db65d348146cf233b2
oguzhanaltan <hoaltan@hotmail.com>
2014-07-25 01:06:57 +0300

    backup

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -130,17 +130,9 @@ if(strlen($subdomain)>0 && !in_array($subdomain,array('www'))){
 
 
 //Bu condition tamamen kaldırılacak.Local de de login olabilmemizi sağlıyor.
-//if($_SERVER['HTTP_HOST']!="127.0.0.1" && $_SERVER['HTTP_HOST']!="localhost") {
-//ini_set('session.cookie_domain', '.clone.gs');
-//}
-
-if(checkdnsrr(env("HTTP_HOST"), "CNAME"))
-        {
-            echo 'looo!';
-        }
-
-session_set_cookie_params(0, '/', env("HTTP_HOST")); 
-session_start();
+if($_SERVER['HTTP_HOST']!="127.0.0.1" && $_SERVER['HTTP_HOST']!="localhost") {
+ini_set('session.cookie_domain', '.clone.gs');
+}
 
 
 
