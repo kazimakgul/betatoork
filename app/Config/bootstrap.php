c6427914dc8e77ab8782c3e79d23b01a714e2fbe
oguzhanaltan <hoaltan@hotmail.com>
2014-07-25 00:33:26 +0300

    no init

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -131,7 +131,7 @@ if(strlen($subdomain)>0 && !in_array($subdomain,array('www'))){
 
 //Bu condition tamamen kaldırılacak.Local de de login olabilmemizi sağlıyor.
 if($_SERVER['HTTP_HOST']!="127.0.0.1" && $_SERVER['HTTP_HOST']!="localhost") {
-ini_set('session.cookie_domain', 'nishgame.nishgame.com');
+//ini_set('session.cookie_domain', '.clone.gs');
 }
 
 
