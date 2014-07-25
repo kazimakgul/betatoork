8397b09270c9d4f1855030bbbd03b5d870d2b01f
Oguzhan Altan <hoaltan@hotmail.com>
2014-07-25 14:59:16 +0300

    clone.gs

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -108,7 +108,7 @@ class AppController extends Controller {
     $this->Cookie->name = 'iqminds_cookie';
     $this->Cookie->time = 0;  // or ’1 hour’
     $this->Cookie->path = '/'; 
-    $this->Cookie->domain = 'nishgame.nishgame.com';
+    $this->Cookie->domain = '.clone.gs';
     $this->Cookie->secure = false;
     $this->Cookie->key = '39lbkutg1i2l0kta6785d8qki5';
     $this->Cookie->httpOnly = true;
