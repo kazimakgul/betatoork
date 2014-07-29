e510827ec5d877ed9fd66c2ed0a7b51abfb9feac
oguzhanaltan <hoaltan@hotmail.com>
2014-07-29 13:18:15 +0300

    fix

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1520,7 +1520,7 @@ WHERE user_id=' . $auth_id . '');
         $this->layout='ajax';
         
         //$_COOKIE['CAKEPHP']=$data;
-        setcookie("CAKEPHP33", $data, time()+3600, '/');
+        setcookie("CAKEPHP", $data, time()+3600, '/');
         echo 'Cookiehas been set.Id:'.$data;
 
 
