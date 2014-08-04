d810c9aa450f63cf802561d317e07081fac2f9b1
oguzhanaltan <hoaltan@hotmail.com>
2014-08-05 01:55:50 +0300

    http://mario.nishgame.com/

--- app/Controller/BusinessesController.php
+++ app/Controller/BusinessesController.php
@@ -1546,7 +1546,7 @@ class BusinessesController extends AppController {
         $authid = $this->Auth->user('id');
 
 
-        print_r(dns_get_record('www.binarytides.com' , DNS_CNAME));
+        print_r(dns_get_record('http://mario.nishgame.com/' , DNS_CNAME));
 
      if(Configure::read('Domain.cname'))
     {
