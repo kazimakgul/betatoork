ea662735c81953f35bb9dbcb37690d0964c09c10
oguzhanaltan <hoaltan@hotmail.com>
2014-08-05 02:08:29 +0300

    test

--- app/Controller/BusinessesController.php
+++ app/Controller/BusinessesController.php
@@ -1546,10 +1546,9 @@ class BusinessesController extends AppController {
         $authid = $this->Auth->user('id');
 
 
-        print_r(dns_get_record('www.socialesman.com' , DNS_CNAME));
-        print_r(dns_get_record('www.socialesman.com' , DNS_ALL));
-        $dns_data=dns_get_record('www.socialesman.com' , DNS_ALL);
-        echo $dns_data[1]['target'].'yess';
+        
+        $dns_data=dns_get_record('www.socialesman.com' , DNS_CNAME)
+        echo $dns_data[0]['target'].'yess';
 
      if(Configure::read('Domain.cname'))
     {
