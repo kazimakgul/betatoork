91f01450243935b44d429baec1de3f43510a8249
oguzhanaltan <hoaltan@hotmail.com>
2014-08-09 00:26:35 +0300

    ,'sort' => 'id', 'direction' => 'desc'

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -146,9 +146,9 @@ if (
         Router::connect('/', array('controller' => 'businesses', 'action' => 'mysite'));
         Router::connect('/play/:seo_url', array('controller' => 'businesses', 'action' => 'play'), array('seo_url' => '[-a-z0-9]+', 'pass' => array('seo_url')));
         Router::connect('/category/:cat_url', array('controller' => 'businesses', 'action' => 'category'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
-        Router::connect('/category/:cat_url/*', array('controller' => 'businesses', 'action' => 'category'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
-        Router::connect('/category/:cat_url/newest', array('controller' => 'businesses', 'action' => 'category'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url','sort' => 'id', 'direction' => 'desc')));
-        Router::connect('/category/:cat_url/newest/*', array('controller' => 'businesses', 'action' => 'category'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url','sort' => 'id', 'direction' => 'desc')));
+        Router::connect('/category/*/*', array('controller' => 'businesses', 'action' => 'category'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
+        Router::connect('/category/:cat_url/newest', array('controller' => 'businesses', 'action' => 'category','sort' => 'id', 'direction' => 'desc'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
+        Router::connect('/category/:cat_url/newest/*', array('controller' => 'businesses', 'action' => 'category','sort' => 'id', 'direction' => 'desc'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
         Router::connect('/category/:cat_url/oldest', array('controller' => 'businesses', 'action' => 'category','sort' => 'id', 'direction' => 'asc'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
         Router::connect('/category/:cat_url/oldest/*', array('controller' => 'businesses', 'action' => 'category','sort' => 'id', 'direction' => 'asc'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
         Router::connect('/category/:cat_url/a-z', array('controller' => 'businesses', 'action' => 'category','sort' => 'name', 'direction' => 'asc'), array('cat_url' => '[-a-z0-9]+', 'pass' => array('cat_url')));
