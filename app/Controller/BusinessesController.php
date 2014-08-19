dff7178236726d42449fcf06f49ae0cd7215d561
Volkan <volkanceliloglu@hotmail.com>
2014-08-19 17:23:23 +0300

    fixed domains

--- app/Controller/BusinessesController.php
+++ app/Controller/BusinessesController.php
@@ -1534,7 +1534,6 @@ class BusinessesController extends AppController {
     public function mysite($userid = NULL) {
         $this->layout = 'Business/business';
         $authid = $this->Auth->user('id');
-
         if (Configure::read('Domain.cname')) {
             $cdomain = Configure::read('Domain.c_root');
 
@@ -1548,7 +1547,7 @@ class BusinessesController extends AppController {
                 $subdomain = Configure::read('Domain.subdomain');
                 if ($subdomain == 'domains') {
                     $this->layout = 'ajax';
-                    $this->render('Elements/business/howtoomap');
+                    $this->render('/Elements/business/howtoomap');
                 }
                 $user_data = $this->User->find('first', array('contain' => false, 'conditions' => array('User.seo_username' => $subdomain), 'fields' => array('User.id')));
                 $userid = $user_data['User']['id'];
