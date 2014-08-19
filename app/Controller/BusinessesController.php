125eb82c24a516150d74435946c4a89d110956b4
Volkan <volkanceliloglu@hotmail.com>
2014-08-19 18:02:25 +0300

    edit

--- app/Controller/BusinessesController.php
+++ app/Controller/BusinessesController.php
@@ -1535,16 +1535,15 @@ class BusinessesController extends AppController {
         $this->layout = 'Business/business';
         $authid = $this->Auth->user('id');
         $subdomain = Configure::read('Domain.subdomain');
-        if($subdomain == "domains"){
-            $this->layout = 'ajax';
-            $this->autoRender = false;
-            $this->render('/Elements/business/howtoomap');
-        }
         if (Configure::read('Domain.cname')) {
             $cdomain = Configure::read('Domain.c_root');
 
             if ($userid == NULL) {
-
+                if($subdomain == "domains"){
+                    $this->layout = 'ajax';
+                    //$this->autoRender = false;
+                    $this->render('/Elements/business/howtoomap');
+                }
                 $user_data = $this->Game->query('SELECT * from custom_domains WHERE domain ="' . $cdomain . '"');
                 $userid = $user_data[0]['custom_domains']['user_id'];
             }
